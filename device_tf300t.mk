76c4d7e565cf7ca95aff2ba32b3f6e4b83ba88c9
Glenn Schuurman <glennschuurman@gmail.com>
2018-05-03 19:23:26 +0200

    changed device full to lineage and reverted lineage.mk to device_tf300t

---
+++ device_tf300t.mk
@@ -0,0 +1,155 @@
+# Copyright (C) 2011 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+$(call inherit-product-if-exists, vendor/asus/tf300t/tf300t-vendor.mk)
+
+DEVICE_PACKAGE_OVERLAYS += device/asus/tf300t/overlay
+
+ifneq ($(TARGET_KERNEL_BUILT_FROM_SOURCE), true)
+# Use prebuilt kernel
+ifeq ($(TARGET_PREBUILT_KERNEL),)
+LOCAL_KERNEL := device/asus/tf300t/kernel
+else
+LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
+endif
+endif
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_KERNEL):kernel
+
+# Files needed for boot image
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/ramdisk/init.cardhu.rc:root/init.cardhu.rc \
+    $(LOCAL_PATH)/ramdisk/init.tf.rc:root/init.tf.rc \
+    $(LOCAL_PATH)/ramdisk/ueventd.cardhu.rc:root/ueventd.cardhu.rc \
+    $(LOCAL_PATH)/ramdisk/init.cardhu.usb.rc:root/init.cardhu.usb.rc \
+    $(LOCAL_PATH)/ramdisk/init.cardhu.cpu.rc:root/init.cardhu.cpu.rc \
+    $(LOCAL_PATH)/ramdisk/fstab.cardhu:root/fstab.cardhu
+
+# Input device config files
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/idc/atmel-maxtouch.idc:system/usr/idc/atmel-maxtouch.idc \
+    $(LOCAL_PATH)/idc/elan-touchscreen.idc:system/usr/idc/elan-touchscreen.idc \
+    $(LOCAL_PATH)/idc/elantech_touchscreen.idc:system/usr/idc/elantech_touchscreen.idc \
+    $(LOCAL_PATH)/idc/raydium_ts.idc:system/usr/idc/raydium_ts.idc \
+    $(LOCAL_PATH)/idc/panjit_touch.idc:system/usr/idc/panjit_touch.idc \
+    $(LOCAL_PATH)/keychars/asusec.kcm:system/usr/keychars/Generic.kcm \
+    $(LOCAL_PATH)/keylayout/asusec.kl:system/usr/keylayout/Generic.kl \
+    $(LOCAL_PATH)/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl
+
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/tablet_core_hardware.xml:system/etc/permissions/tablet_core_hardware.xml \
+    frameworks/native/data/etc/android.hardware.bluetooth.xml:system/etc/permissions/android.hardware.bluetooth.xml \
+    frameworks/native/data/etc/android.hardware.camera.xml:system/etc/permissions/android.hardware.camera.xml \
+    frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
+    frameworks/native/data/etc/android.hardware.camera.autofocus.xml:system/etc/permissions/android.hardware.camera.autofocus.xml \
+    frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
+    frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+    frameworks/native/data/etc/android.hardware.ethernet.xml:system/etc/permissions/android.hardware.ethernet.xml \
+    frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
+    frameworks/native/data/etc/android.hardware.sensor.gyroscope.xml:system/etc/permissions/android.hardware.sensor.gyroscope.xml \
+    frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
+    frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
+    frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
+    frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
+    frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
+    frameworks/native/data/etc/android.software.freeform_window_management.xml:system/etc/permissions/android.software.freeform_window_management.xml
+
+# Build characteristics setting
+PRODUCT_CHARACTERISTICS := tablet
+
+# Extra packages to build for this device
+PRODUCT_PACKAGES += \
+    setup_fs \
+    audio.a2dp.default \
+    audio.r_submix.default \
+    tinymix \
+    libaudioutils \
+    blobpack_tfp \
+    mkfs.f2fs \
+    wifimacwriter
+
+PRODUCT_PACKAGES +=  libstlport
+
+PRODUCT_PACKAGES += \
+   libnetcmdiface
+
+ PRODUCT_PACKAGES += \
+    libwpa_client \
+    hostapd \
+    wpa_supplicant \
+    wpa_supplicant.conf
+
+PRODUCT_PACKAGES += power.cardhu
+
+# Propertys spacific for this device
+PRODUCT_PROPERTY_OVERRIDES := \
+    wifi.interface=wlan0 \
+    wifi.supplicant_scan_interval=15 \
+    tf.enable=y \
+    ro.opengles.version=131072 \
+    persist.sys.usb.config=mtp,adb
+
+# libhwui flags
+PRODUCT_PROPERTY_OVERRIDES += \
+    debug.hwui.render_dirty_regions=false
+
+# Tegra 3 spacific overrides
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.tegra.nvmmlite=1
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.external_drive_world_rw=1
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    display.smart_dimmer=1
+
+# Prime spacific overrides
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.epad.model=TF300T \
+    ro.product.model=TF300T
+# Aggresively optimize art for performance
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.dex2oat-flags=--no-watch-dog \
+    dalvik.vm.dex2oat-swap=false \
+    pm.dexopt.bg-dexopt=everything-profile \
+    pm.dexopt.ab-ota=everything-profile \
+    pm.dexopt.nsys-library=everything-profile \
+    pm.dexopt.core-app=everything-profile
+
+# media files
+PRODUCT_COPY_FILES += \
+    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
+    device/asus/tf300t/media_codecs.xml:system/etc/media_codecs.xml \
+    device/asus/tf300t/media_profiles.xml:system/etc/media_profiles.xml \
+    device/asus/tf300t/audio_policy.conf:system/etc/audio_policy.conf
+
+# wifi files
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf
+
+# gps config files
+PRODUCT_COPY_FILES += \
+    device/asus/tf300t/gpsconfig.xml:system/etc/gps/gpsconfig.xml \
+    device/asus/tf300t/gps.conf:system/etc/gps.conf \
+    device/asus/tf300t/bt_vendor.conf:system/etc/bluetooth/bt_vendor.conf
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/prebuilt/xbin/rsync:system/xbin/rsync
+
+# Inherit tablet dalvik settings
+$(call inherit-product, frameworks/native/build/tablet-dalvik-heap.mk)
--- lineage.mk
+++ lineage.mk
@@ -4,7 +4,7 @@
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-# http://www.apache.org/licenses/LICENSE-2.0
+#      http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -12,144 +12,25 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-$(call inherit-product-if-exists, vendor/asus/tf300t/tf300t-vendor.mk)
+# Inherit from those products. Most specific first.
+$(call inherit-product, device/asus/tf300t/device_tf300t.mk)
+# This is where we'd set a backup provider if we had one
+#$(call inherit-product, device/sample/products/backup_overlay.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 
-DEVICE_PACKAGE_OVERLAYS += device/asus/tf300t/overlay
+# Set the physical display size with PRODUCT_AAPT_CONFIG
+# Options are small(~2"-3"), normal(~3"-4"), large(~4"-7"), xlarge(~7" and larger)
+PRODUCT_AAPT_CONFIG := xlarge
+# Set the preferred asset density with PRODUCT_AAPT_PREF_CONFIG
+# See android CCD for valid options
+PRODUCT_AAPT_PREF_CONFIG := mdpi
+# A list of dpis to select prebuilt apk, in precedence order.
+# See android CCD for valid options
+PRODUCT_AAPT_PREBUILT_DPI := mdpi ldpi tvdpi hdpi xhdpi
 
-ifneq ($(TARGET_KERNEL_BUILT_FROM_SOURCE), true)
-# Use prebuilt kernel
-ifeq ($(TARGET_PREBUILT_KERNEL),)
-LOCAL_KERNEL := device/asus/tf300t/kernel
-else
-LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
-endif
-endif
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_KERNEL):kernel
-
-# Files needed for boot image
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/ramdisk/init.cardhu.rc:root/init.cardhu.rc \
-    $(LOCAL_PATH)/ramdisk/init.tf.rc:root/init.tf.rc \
-    $(LOCAL_PATH)/ramdisk/ueventd.cardhu.rc:root/ueventd.cardhu.rc \
-    $(LOCAL_PATH)/ramdisk/init.cardhu.usb.rc:root/init.cardhu.usb.rc \
-    $(LOCAL_PATH)/ramdisk/init.cardhu.cpu.rc:root/init.cardhu.cpu.rc \
-    $(LOCAL_PATH)/ramdisk/fstab.cardhu:root/fstab.cardhu
-
-# Input device config files
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/idc/atmel-maxtouch.idc:system/usr/idc/atmel-maxtouch.idc \
-    $(LOCAL_PATH)/idc/elan-touchscreen.idc:system/usr/idc/elan-touchscreen.idc \
-    $(LOCAL_PATH)/idc/elantech_touchscreen.idc:system/usr/idc/elantech_touchscreen.idc \
-    $(LOCAL_PATH)/idc/raydium_ts.idc:system/usr/idc/raydium_ts.idc \
-    $(LOCAL_PATH)/idc/panjit_touch.idc:system/usr/idc/panjit_touch.idc \
-    $(LOCAL_PATH)/keychars/asusec.kcm:system/usr/keychars/Generic.kcm \
-    $(LOCAL_PATH)/keylayout/asusec.kl:system/usr/keylayout/Generic.kl \
-    $(LOCAL_PATH)/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl
-
-
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/tablet_core_hardware.xml:system/etc/permissions/tablet_core_hardware.xml \
-    frameworks/native/data/etc/android.hardware.bluetooth.xml:system/etc/permissions/android.hardware.bluetooth.xml \
-    frameworks/native/data/etc/android.hardware.camera.xml:system/etc/permissions/android.hardware.camera.xml \
-    frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
-    frameworks/native/data/etc/android.hardware.camera.autofocus.xml:system/etc/permissions/android.hardware.camera.autofocus.xml \
-    frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
-    frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
-    frameworks/native/data/etc/android.hardware.ethernet.xml:system/etc/permissions/android.hardware.ethernet.xml \
-    frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
-    frameworks/native/data/etc/android.hardware.sensor.gyroscope.xml:system/etc/permissions/android.hardware.sensor.gyroscope.xml \
-    frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
-    frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
-    frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
-    frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
-    frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
-    frameworks/native/data/etc/android.software.freeform_window_management.xml:system/etc/permissions/android.software.freeform_window_management.xml
-
-# Build characteristics setting
-PRODUCT_CHARACTERISTICS := tablet
-
-# Extra packages to build for this device
-PRODUCT_PACKAGES += \
-    setup_fs \
-    audio.a2dp.default \
-    audio.r_submix.default \
-    tinymix \
-    libaudioutils \
-    blobpack_tfp \
-    mkfs.f2fs \
-    wifimacwriter
-
-PRODUCT_PACKAGES +=  libstlport
-
-PRODUCT_PACKAGES += \
-   libnetcmdiface
-
- PRODUCT_PACKAGES += \
-    libwpa_client \
-    hostapd \
-    wpa_supplicant \
-    wpa_supplicant.conf
-
-PRODUCT_PACKAGES += power.cardhu
-
-# Propertys spacific for this device
-PRODUCT_PROPERTY_OVERRIDES := \
-    wifi.interface=wlan0 \
-    wifi.supplicant_scan_interval=15 \
-    tf.enable=y \
-    ro.opengles.version=131072 \
-    persist.sys.usb.config=mtp,adb
-
-# libhwui flags
-PRODUCT_PROPERTY_OVERRIDES += \
-    debug.hwui.render_dirty_regions=false
-
-# Tegra 3 spacific overrides
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.tegra.nvmmlite=1
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.external_drive_world_rw=1
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    display.smart_dimmer=1
-
-# Prime spacific overrides
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.epad.model=TF300T \
-    ro.product.model=TF300T
-# Aggresively optimize art for performance
-PRODUCT_PROPERTY_OVERRIDES += \
-    dalvik.vm.dex2oat-flags=--no-watch-dog \
-    dalvik.vm.dex2oat-swap=false \
-    pm.dexopt.bg-dexopt=everything-profile \
-    pm.dexopt.ab-ota=everything-profile \
-    pm.dexopt.nsys-library=everything-profile \
-    pm.dexopt.core-app=everything-profile
-
-# media files
-PRODUCT_COPY_FILES += \
-    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
-    frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
-    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
-    device/asus/tf300t/media_codecs.xml:system/etc/media_codecs.xml \
-    device/asus/tf300t/media_profiles.xml:system/etc/media_profiles.xml \
-    device/asus/tf300t/audio_policy.conf:system/etc/audio_policy.conf
-
-# wifi files
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf
-
-# gps config files
-PRODUCT_COPY_FILES += \
-    device/asus/tf300t/gpsconfig.xml:system/etc/gps/gpsconfig.xml \
-    device/asus/tf300t/gps.conf:system/etc/gps.conf \
-    device/asus/tf300t/bt_vendor.conf:system/etc/bluetooth/bt_vendor.conf
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/prebuilt/xbin/rsync:system/xbin/rsync
-
-# Inherit tablet dalvik settings
-$(call inherit-product, frameworks/native/build/tablet-dalvik-heap.mk)
+# Discard inherited values and use our own instead.
+PRODUCT_NAME := lineage_tf300t
+PRODUCT_DEVICE := tf300t
+PRODUCT_BRAND := Asus
+PRODUCT_MODEL := TF300T
+PRODUCT_RESTRICT_VENDOR_FILES := false
